a1f64868e709641b41d097ec8e5a354fe1ebfd13
Alexander Brandbyge <alexander@brandbyge.dk>
2024-03-13 23:39:46 +0100

    forces now have ceasfire and friendly set

--- src/mods/bitermaul/control.lua
+++ src/mods/bitermaul/control.lua
@@ -1,11 +1,15 @@
 require("map_generation")
+require("forces")
+
 
 local function oninit(event)
   hook_map_gen()
+  setup_forces()
 end
 
 local function onload(event)
-  hook_map_gen()  
+  hook_map_gen()
+  setup_forces()
 end
 
 if script.level.level_name == "bitermaul" then
